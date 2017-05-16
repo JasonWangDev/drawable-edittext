c15c465ab5a480793701b8ee728ae4dada5beeb8
DOITSKY\jason <jasonwang@doitsky.com>
2017-05-16 15:42:32 +0800

    新增 DrawableCheckBox 元件 & 修改專案名稱

---
+++ app/src/main/java/idv/jasonwang/widgetutils/sample/SampleActivity.java
@@ -0,0 +1,34 @@
+package idv.jasonwang.widgetutils.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import idv.jasonwang.widgetutils.DrawableCheckBox;
+import idv.jasonwang.widgetutils.DrawableEditText;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((DrawableEditText) findViewById(R.id.drawableEditText)).setDrawableBottomSize(100, 100);
+                ((DrawableEditText) findViewById(R.id.drawableEditText)).setDrawableBottom(android.R.drawable.ic_menu_call);
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((DrawableCheckBox) findViewById(R.id.drawableCheckBox)).setDrawableLeftSize(15, 15);
+                ((DrawableCheckBox) findViewById(R.id.drawableCheckBox)).setDrawableLeft(android.R.drawable.ic_menu_call);
+            }
+        });
+    }
+
+}
